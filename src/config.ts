977e182427cb65d53325149aaec7769545e883ce
Siddarth Malapati <155690174+smalapati66@users.noreply.github.com>
2026-01-22 23:04:45 -0600

    added avatars for roommates

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,25 @@
 export const HOUSE_ID = "6f67fe2a-bb29-44ba-b4d9-bf50eda4ac78";
 
-export const USER_LABELS : Record<string, string> = {
-  "3547a8d5-4ceb-4cef-adcf-5149dad5a96c" : "Sid",
-  "7f8200b1-dc35-474e-816c-9f0084d570fa" : "Diego",
-  "8dd3db38-59b2-469f-a27d-02a3f6be95bd" : "Clyde",
-  "f4e347a8-27db-429b-9cf6-b53eae6158f7" : "Tanure",
+export const USER_PROFILES : Record<string, {
+  name: string,
+  avatarSrc: string,
+}> = {
+  "3547a8d5-4ceb-4cef-adcf-5149dad5a96c" : {
+    name: "Sid",
+    avatarSrc: "/src/assets/avatars/Sid.jpg"
+  },
+  "7f8200b1-dc35-474e-816c-9f0084d570fa" : {
+    name: "Diego",
+    avatarSrc: "/src/assets/avatars/Diego.jpg"
+  },
+  "8dd3db38-59b2-469f-a27d-02a3f6be95bd" : {
+    name: "Clyde",
+    avatarSrc: "/src/assets/avatars/Clyde.jpg"
+  },
+  "f4e347a8-27db-429b-9cf6-b53eae6158f7" : {
+    name: "Tanure",
+    avatarSrc: "/src/assets/avatars/Tanure.jpg"
+  },
 }
 
 export const USER_TO_NEXT : Record<string, string> = {
